a711bca00b6ecb3fe3f23909bcbc2e29ba7812f6
David Anwar <hapidun10@yahoo.co.id>
2020-04-23 06:31:39 +0700

    implemented the user tab

--- app/src/main/java/com/example/instagramclone/ProfileTab.java
+++ app/src/main/java/com/example/instagramclone/ProfileTab.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
+
+    EditText editProfileName, editProfileBio, editProfileHobby, editProfileSport, editProfileProfession;
+    private Button btnUpdateInfo;
+
+
 
     public ProfileTab() {
         // Required empty public constructor
@@ -23,6 +36,56 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        // harus menggunakan view agar bisa menginisialisasi id
+        editProfileName = view.findViewById(R.id.editProfileName);
+        editProfileBio = view.findViewById(R.id.editProfileBio);
+        editProfileProfession = view.findViewById(R.id.editProfileProfession);
+        editProfileHobby = view.findViewById(R.id.editProfileHoby);
+        editProfileSport = view.findViewById(R.id.editProfileSport);
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateProfile);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        // jika data server null maka isi dengan ""
+        // jika tidak menggunakan methode ini maka akan menampilkan string "null" di edit text
+        if (parseUser.get("profileName") == null){
+            editProfileName.setText("");
+        } else {
+            editProfileName.setText(parseUser.get("profileName") + "");
+        }
+
+        // jika menggunakn toString() maka akan crash jika data yang ambil dari server kosong
+        editProfileBio.setText(parseUser.get("profileBio") + "");
+        editProfileProfession.setText(parseUser.get("profileProfession") + "");
+        editProfileHobby.setText(parseUser.get("profileHobby") + "");
+        editProfileSport.setText(parseUser.get("profileSport") + "");
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put("profileName", editProfileName.getText().toString());
+                parseUser.put("profileBio", editProfileBio.getText().toString());
+                parseUser.put("profileProfession", editProfileProfession.getText().toString());
+                parseUser.put("profileHobby", editProfileHobby.getText().toString());
+                parseUser.put("profileSport", editProfileSport.getText().toString());
+//                ProgressDialog progressDialog = new ProgressDialog(getContext());
+//                progressDialog
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null){
+                            FancyToast.makeText(getContext(), "Info Updated",
+                                    FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                        } else {
+                            FancyToast.makeText(getContext(), e.getMessage(),
+                                    FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                        }
+                    }
+                });
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/instagramclone/UserTab.java
+++ app/src/main/java/com/example/instagramclone/UserTab.java
@@ -7,12 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserTab extends Fragment {
+
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public UserTab() {
         // Required empty public constructor
@@ -23,6 +38,38 @@ public class UserTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_tab, container, false);
+        final View view = inflater.inflate(R.layout.fragment_user_tab, container, false);
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+
+        // butuh array adapter sebagai MCV karena arrayList tidak bisa langsung berinteraksi dengan struktur database
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+        final TextView textLoadingUser = view.findViewById(R.id.textLoadingUser);
+
+        // menampilkan data user yang diambil dari server
+        ParseQuery<ParseUser> parseUserParseQuery = ParseUser.getQuery();
+
+        // agar current user tidak ditampilkan datanya
+        parseUserParseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        parseUserParseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null){
+                    if (objects.size() > 0){
+                        for (ParseUser user : objects){
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+
+                        // text loading akan hilang setelah 2 second dan digantikan dengan listView
+                        textLoadingUser.animate().alpha(0).setDuration(2000);
+                        // list view muncul setelah textLoading hilang
+                        listView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 }
